0aebf947e8c1b1573fbcd325be3040ee03bd8328
Pomuditha Umayangi <166549287+PomudithaUmayangi@users.noreply.github.com>
2025-03-21 13:58:12 +0530

    Update external-script.js

--- external-script.js
+++ external-script.js
@@ -1,10 +1,10 @@
 // Simple external JS file for testing
 
-console.log("External script loaded successfully!");
+console.log("External script loaded successfully as hacker one tester!");
 
 // Function to show an alert
 function showMessage() {
-    alert("External Script Executed Successfully!");
+    alert("External Script Executed Successfully as hacker one tester !");
 }
 
 // Call the function to show the alert
