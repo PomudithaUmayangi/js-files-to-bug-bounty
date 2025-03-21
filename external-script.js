fa59d7967fd53f0223c866ad034d5c29f97172c2
PomudithaUmayangi <umayangirajapaksha2019@gmail.com>
2025-03-21 13:48:29 +0530

    external script tesing

---
+++ external-script.js
@@ -0,0 +1,11 @@
+// Simple external JS file for testing
+
+console.log("External script loaded successfully!");
+
+// Function to show an alert
+function showMessage() {
+    alert("External Script Executed Successfully!");
+}
+
+// Call the function to show the alert
+showMessage();
